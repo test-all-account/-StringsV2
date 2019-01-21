8a9c412f76d2577810ce251863a6f33ee7a9d16c
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2019-01-21 17:12:27 +0300

    Add project files.

---
+++ Tests/PublicTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace StringHelpers.Tests
+{
+    [TestFixture]
+    public class PublicTest
+    {
+        [TestCase("abracadabra", ExpectedResult = 5)]
+        [TestCase("sprppr", ExpectedResult = 0)]
+        [TestCase("aaa", ExpectedResult = 3)]
+        public int GetVowelCountTest(string source) => VowelCountTask.GetVowelCount(source);
+    }
+}
---
+++ VowelCountTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringHelpers
+{
+    public class VowelCountTask
+    {
+        public static int GetVowelCount(string str)
+        {
+            // put your code here
+            throw new NotImplementedException();
+        }
+    }
+}
